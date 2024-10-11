ea448a681d16dc43e885be677175b39cbe7ec7cf
Eka Wahyu <94496984+kwhyu@users.noreply.github.com>
2024-10-11 10:56:47 +0800

    Update responses.py

--- responses.py
+++ responses.py
@@ -12,10 +12,10 @@ def get_response(user_input: str) -> str:
         return 'oi oi oi oi'
     elif 'hello' in lowered:
         return '???'
-    elif '!bye' in lowered:
-        return 'See you!'
-    elif '!roll dice' in lowered:
-        return f'You rolled: {randint(1, 6)}'
+    elif 'hbd' in lowered:
+        return 'HBD OI OI OI OI'
+    elif 'p adu' in lowered:
+        return 'HUH !?'
     else:
         return choice(['',
                        '',
