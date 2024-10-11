6efbec8ed634619f072db145efd5c648029b35a1
Eka Wahyu <94496984+kwhyu@users.noreply.github.com>
2024-10-11 10:52:32 +0800

    Update responses.py

--- responses.py
+++ responses.py
@@ -7,11 +7,11 @@ def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
 
     if lowered == '':
-        return 'Well, you\'re awfully silent...'
+        return 'apa tod'
+    elif 'halo' in lowered:
+        return 'oi oi oi oi'
     elif 'hello' in lowered:
-        return 'Hello there!'
-    elif '!how are you' in lowered:
-        return 'Good, thanks!'
+        return '???'
     elif '!bye' in lowered:
         return 'See you!'
     elif '!roll dice' in lowered:
